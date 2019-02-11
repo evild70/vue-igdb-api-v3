ab354787887a2b9eb3063986de217a35e20d7d4c
Dennis Lahay <dlahay@gmail.com>
2019-02-11 10:48:13 -0600

    Formatting

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <template>
-  <div id="app">
-      <h3>Check the console for your data.</h3>
-      <hr />
-      <HelloWorld />
-  </div>
+    <div id="app">
+        <h3>Check the console for your data.</h3>
+        <hr />
+        <HelloWorld />
+    </div>
 </template>
 
 <script>
@@ -12,39 +12,42 @@ import HelloWorld from '@/components/HelloWorld';
 
 export default {
     components: { HelloWorld },
-  mounted() {
-    axios({
-      url: "http://localhost:[PORT_NUMBER]/games",
-      method: 'POST',
-      headers: {
-        'Accept': 'application/json',
-        'user-key': '[API_KEY]'
-      },
-      data: 'fields name; limit 10;'
-    })
-    .then(response => {
-      console.log(response.data);
-    })
-    .catch(err => {
-      console.error(err);
-    });
+    mounted() {
+        axios({
+            url: "http://localhost:[PORT_NUMBER]/games",
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'user-key': '[API_KEY]'
+            },
+            data: 'fields name; limit 10;'
+        })
+        .then(response => {
+            console.log(response.data);
+        })
+        .catch(err => {
+            console.error(err);
+        });
 
-    // If you don't want to use axios (it's easier IMHO),
-    // you can use the Fetch API (https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
-    // fetch('http://localhost:[PORT_NUMBER]/games', {
-    //   method: 'GET',
-    //   headers: {
-    //     'Content-type': 'application/json',
-    //     'user-key': '[API_KEY]',
-    //   },
-    // //   body: 'fields name; limit 10;'
-    // }).then(response => {
-    //     return response.json();
-    // }).then(data => {
-    //     console.log(data);
-    // }).catch(err => {
-    //     console.error(err);
-    // });
-  }
+        // If you don't want to use axios (it's easier IMHO),
+        // you can use the Fetch API (https://developer.mozilla.org/en-US/docs/Web/API/Fetch_API/Using_Fetch)
+        // fetch('http://localhost:[PORT_NUMBER]/games', {
+        //     method: 'GET',
+        //     headers: {
+        //         'Content-type': 'application/json',
+        //         'user-key': '[API_KEY]',
+        //     },
+        //     body: 'fields name; limit 10;'
+        // })
+        // .then(response => {
+        //     return response.json();
+        // })
+        // .then(data => {
+        //     console.log(data);
+        // })
+        // .catch(err => {
+        //     console.error(err);
+        // });
+    }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,13 @@
 // Vue CLI uses http-proxy-middleware (https://github.com/chimurai/http-proxy-middleware#options)
 // These are the options that worked for me. YMMV.
 module.exports = {
-  devServer: {
-    proxy: {
-      '^/': {
-        target: 'https://api-v3.igdb.com/',
-        ws: false,
-        changeOrigin: true
-      }
+    devServer: {
+        proxy: {
+            '^/': {
+                target: 'https://api-v3.igdb.com/',
+                ws: false,
+                changeOrigin: true
+            }
+        }
     }
-  }
 }
